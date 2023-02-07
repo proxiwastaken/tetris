91bc2cce38fe97611d9554b23e5c519e387a9c88
Soproxi <zoey.mccall25@gmail.com>
2023-02-07 05:03:26 +0000

    added cube and boilerplate code

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -13,13 +13,21 @@ namespace Tetris
 {
     public partial class Form1 : Form
     {
-        Button[,] gridBtn = new Button[10,20];
+        public static int xLength = 10;
+        private static int yLength = 20;
+        Button[,] gridBtn = new Button[xLength,yLength];
         Label scoreLbl = new Label();
         public int score = 0;
+
+        public int BlockStartX = xLength / 2 - 1; // 4, start blocks here and build out right.
+        public int BlockStartY = 0; // Top of grid
         public Form1()
         {
             InitializeComponent();
+
+            // Variables
             
+
             // Initialise Grid
             for (int x = 0; x < gridBtn.GetLength(0); x++) 
             {
@@ -29,7 +37,7 @@ namespace Tetris
                     gridBtn[x,y].FlatStyle = FlatStyle.Flat;
                     gridBtn[x, y].FlatAppearance.BorderColor = Color.White;
                     gridBtn[x,y].SetBounds(40+(30 * x), 60+(30 * y), 30, 30);
-                    gridBtn[x,y].BackColor = Color.PowderBlue;
+                    gridBtn[x,y].BackColor = Color.PowderBlue; // Every "Powder Blue" cell is empty.
                     // gridBtn[x,y].Text = Convert.ToString(x + 1) + "," + (y+1);
 
                     gridBtn[x,y].Click += new EventHandler(this.btnEvent_Click);
@@ -45,21 +53,89 @@ namespace Tetris
             // GAME START
 
             // Creates a new block at the top of the grid.
-            gridBtn = CreateNewBlock(gridBtn);
+
+            // Square
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    gridBtn[BlockStartX+x, BlockStartY+y].BackColor = Color.Coral;
+                }
+            }
+
         }
 
-        Button[,] CreateNewBlock(Button[,] grid)
+        private void ClearRow(int x)
         {
-            Button insertLocation = grid[4, 0];
-            // Buttons
+            for (int y = 0; y < yLength; y++)
+            {
+                gridBtn[x,y].BackColor = Color.PowderBlue;
+            }
+        }
 
-            //Square
-            public int[,] SquareData;
+        private void MoveDown(int x, int numRows)
+        {
+            for (int y = 0; y < yLength; y++)
+            {
+                gridBtn[x + numRows, y] = gridBtn[x,y];
+                gridBtn[x, y].BackColor = Color.PowderBlue;
+            }
+        }
 
-                //Insert square
-            
-            
-            return grid;
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+
+            for (int x = xLength - 1; x >= 0; x--)
+            {
+                if (IsRowFull(x))
+                {
+                    ClearRow(x);
+                    cleared++;
+                } else if (cleared > 0)
+                {
+                    MoveDown(x, cleared);
+                }
+            }
+
+            return cleared;
+        }
+
+        public bool IsInside(int x, int y)
+        {
+            return x >= 0 && x < xLength && y >= 0 && y < yLength;
+        }
+
+        public bool IsEmpty(int x, int y)
+        {
+            return IsInside(x, y) && gridBtn[x, y].BackColor.Equals(Color.PowderBlue);
+        }
+
+        public bool IsRowEmpty(int x)
+        {
+            for (int y = 0; y < yLength; y++)
+            {
+                if (!(gridBtn[x, y].BackColor.Equals(Color.PowderBlue)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsRowFull(int x)
+        {
+            for (int y = 0; y < yLength; y++)
+            {
+                if (gridBtn[x, y].BackColor.Equals(Color.PowderBlue))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         void btnEvent_Click(object sender, EventArgs e)
